1fe70e77f834fb8aff207c2a70b3273ac3d0fe34
David M. Moreno <david.mmoreno@gmail.com>
2024-05-16 21:51:31 -0600

    Initial commit. Adding UI with streamlit examples

---
+++ ui/app.py
@@ -0,0 +1,112 @@
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+import time
+
+st.header("Executor")
+
+
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+
+st.write("Printing the table with 'magic'")
+df
+
+
+st.write("Printing the table with the 'table' widget")
+st.table(df)
+
+
+st.write("Protting a line chart of random dataframe")
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+st.line_chart(chart_data)
+
+
+st.write("Now, displaying dots in a map")
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+st.map(map_data)
+
+
+st.write("Using widgets")
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
+
+st.text_input("Your name", key="name")
+# You can access the value at any point with:
+st.session_state.name
+
+
+st.write("Using Checkboxes")
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+    chart_data
+
+
+st.write("Using slectbox for options")
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+    })
+option = st.selectbox(
+    'Which number do you like best?',
+     df['first column'])
+'You selected: ', option
+
+
+st.write("Control the layout of your app")
+left_column, right_column = st.columns(2)
+# You can use a column just like st.sidebar:
+left_column.button('Press me!')
+
+# Or even better, call Streamlit functions inside a "with" block:
+with right_column:
+    chosen = st.radio(
+        'Sorting hat',
+        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
+    st.write(f"You are in {chosen} house!")
+
+
+
+
+
+
+with st.sidebar:
+    st.write("This is my sidebar")
+
+    add_selectbox = st.selectbox(
+        'How would you like to be contacted?',
+        ('Email', 'Home phone', 'Mobile phone')
+    )
+
+
+    add_slider = st.slider(
+        'Select a range of values',
+        0.0, 100.0, (25.0, 75.0)
+    )
+
+
+    'Starting a long computation...'
+    # Add a placeholder
+    latest_iteration = st.empty()
+    bar = st.progress(0)
+
+    for i in range(100):
+      # Update the progress bar with each iteration.
+      latest_iteration.text(f'Iteration {i+1}')
+      bar.progress(i + 1)
+      time.sleep(0.1)
+    '...and now we\'re done!'
